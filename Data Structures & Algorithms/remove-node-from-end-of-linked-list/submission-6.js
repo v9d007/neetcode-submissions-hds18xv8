5bcba74a3133b5224317d4f51ecb837ff21cafad
Vinod Kumar Singh <130828719+v9d007@users.noreply.github.com>
2026-04-03 15:23:28 +0530

    Add: remove-node-from-end-of-linked-list - submission-6

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+  /**
+   * @param {ListNode} head
+   * @param {number} n
+   * @return {ListNode}
+   */
+  removeNthFromEnd(head, n) {
+    let dummy = { next: head };
+    let fast = dummy;
+    let slow = dummy;
+
+    console.log(fast)
+
+    // move fast n+1 steps
+    for (let i = 0; i <= n; i++) {
+      fast = fast.next;
+      console.log("i ----",fast)
+    }
+
+    // move both
+    while (fast) {
+        console.log("fast",fast)
+        console.log("slow---",slow)
+      fast = fast.next;
+      slow = slow.next;
+    }
+
+    // delete node
+    slow.next = slow.next.next;
+
+    return dummy.next;
+  }
+}
