738f930a24b309aefc0cd23f02d5d0a84c15c6e7
Vinod Kumar Singh <130828719+v9d007@users.noreply.github.com>
2026-04-06 13:45:37 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let one = 1
+        let two = 0
+
+        for(let i=0; i<n; i++){
+            let temp = one
+            one = two + one
+            two = temp
+        }
+
+        return one
+    }
+}
